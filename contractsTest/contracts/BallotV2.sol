7bfdf939ff4cad8f75b9eeccda4ee26a22ae3587
Nicolas Riquelme <nrriquelme@gmail.com>
2021-04-12 00:39:44 -0400

    Registering voters on mapping

--- contractsTest/contracts/BallotV2.sol
+++ contractsTest/contracts/BallotV2.sol
@@ -4,8 +4,23 @@ contract FactoryBallot {
     string[] proposals;
     bytes[] proposalsConverted;
 
+    address public chairperson;
+
+    struct Voter {
+        string name;
+        string lastname;
+        string email;
+    }
+
+    mapping(address => Voter) public voters;
+
+    constructor() {
+        chairperson = msg.sender;
+    }
+
     function convertToBytes(string memory proposalStored)
         private
+        pure
         returns (bytes memory result)
     {
         bytes memory proposalToBytes = bytes(proposalStored);
@@ -30,6 +45,9 @@ contract FactoryBallot {
         _;
     }
 
+    // function to pass a string for proposal
+    // this string is converted to bytes
+    // TODO: encrypt this or the resulted data
     function setProposal(string memory proposal)
         public
         checkValueNotEmpty(proposal)
@@ -37,7 +55,52 @@ contract FactoryBallot {
         pushToArrayOfBytes(proposal);
     }
 
+    // returns the array of proposals
     function getArrayOfBytes() public view returns (bytes[] memory data) {
         return proposalsConverted;
+    }
+
+    modifier onlyChairperson {
+        require(
+            msg.sender == chairperson,
+            "Only chairperson can register new voters"
+        );
+        _;
+    }
+
+    modifier checkAddressNotEmpty(address v) {
+        require(bytes20(v).length != 0, "No address supplied");
+        _;
+    }
+
+    function registerVoters(
+        address voter,
+        string memory name,
+        string memory lastname,
+        string memory email
+    ) public onlyChairperson checkAddressNotEmpty(voter) {
+        Voter storage v = voters[voter];
+        v.name = name;
+        v.lastname = lastname;
+        v.email = email;
+    }
+
+    function getInfoVoter(address voter)
+        public
+        view
+        onlyChairperson
+        checkAddressNotEmpty(voter)
+        returns (
+            string memory name,
+            string memory lastname,
+            string memory email
+        )
+    {
+        Voter storage v = voters[voter];
+        name = v.name;
+        lastname = v.lastname;
+        email = v.email;
+
+        return (name, lastname, email);
     }
 }
