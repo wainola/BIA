c99e59eeea0b17576f5420461d4133cc3f06236d
Nicolas Riquelme <nrriquelme@gmail.com>
2021-05-02 00:32:02 -0400

    Transfer action testing

--- erc20Token/contracts/ERC20Token.sol
+++ erc20Token/contracts/ERC20Token.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./Context.sol";
+import "./MyContext.sol";
 import "./IERC20.sol";
 
 interface IERC20MetaData is IERC20 {
@@ -49,14 +49,40 @@ contract MyTokenERC20 is Context, IERC20, IERC20MetaData {
         virtual
         override
         returns (uint256)
-    {}
+    {
+        return _balances[account];
+    }
 
     function transfer(address recipient, uint256 amount)
         public
         virtual
         override
         returns (bool)
-    {}
+    {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal virtual {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        _beforeTokenTransfer(sender, recipient, amount);
+
+        uint256 senderBalance = _balances[sender];
+        require(
+            senderBalance >= amount,
+            "ERC20: transfer amount exceeds balance"
+        );
+
+        _balances[sender] = senderBalance - amount;
+        _balances[recipient] += amount;
+
+        emit Transfer(sender, recipient, amount);
+    }
 
     function allowance(address owner, address spender)
         public
--- erc20Token/test/erc20.js
+++ erc20Token/test/erc20.js
@@ -38,5 +38,71 @@ contract("MyTokenERC20", function (accounts) {
         );
       });
     });
+
+    describe("balanceOf", function () {
+      describe("when the requested account has no tokens", function () {
+        it("return zero", async function () {
+          expect(
+            await this.token.balanceOf(anotherAccount)
+          ).to.be.bignumber.equal("0");
+        });
+      });
+
+      describe("when the requested account has some tokens", function () {
+        it("returns the total ammount of tokens", async function () {
+          expect(
+            await this.token.balanceOf(initialHolder)
+          ).to.be.bignumber.equal(initialSupply);
+        });
+      });
+
+      describe("transfer", function () {
+        const transfer = function (from, to, value) {
+          return this.token.transfer(to, value, { from });
+        };
+        describe("when the recipient is not the zero address", function () {
+          describe("and the sender does not have enough balance", function () {
+            const amount = initialSupply.addn(1);
+
+            it("should revert the transfer", async function () {
+              await expectRevert(
+                transfer.call(this, initialHolder, recipient, amount),
+                "transfer amount exceeds balanc"
+              );
+            });
+          });
+
+          describe("when sender transfer all balance", function () {
+            const amount = initialSupply;
+
+            it("transfer the requested amount", async function () {
+              await transfer.call(this, initialHolder, recipient, amount);
+
+              expect(
+                await this.token.balanceOf(initialHolder)
+              ).to.be.bignumber.equal("0");
+              expect(
+                await this.token.balanceOf(recipient)
+              ).to.be.bignumber.equal(amount);
+            });
+
+            it("emits a transfer event", async function () {
+              const { logs } = await transfer.call(
+                this,
+                initialHolder,
+                recipient,
+                amount
+              );
+
+              expectEvent.inLogs(logs, "Transfer", {
+                from: initialHolder,
+                to: recipient,
+                value: amount,
+              });
+            });
+          });
+        });
+      });
+    });
   });
 });
